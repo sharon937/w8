fd1d512cd31336afce41109bb9baf17ae51907e5
陳品璇 <83741454+sharon937@users.noreply.github.com>
2021-11-13 18:48:40 +0800

    Add files via upload

---
+++ TONE_Melody.ino
@@ -0,0 +1,32 @@
+#include "pitches.h"
+
+int freq = 2000;
+int channel = 0;
+int resolution = 8;
+
+int tonePin = 12;
+
+int melody[] = {
+  NOTE_E5, NOTE_A5, NOTE_A5, NOTE_A5, NOTE_G5, NOTE_A5, NOTE_A5,
+  NOTE_E5, NOTE_E5, NOTE_G5, 0,};
+int noteDurations[] = {
+  8, 8, 4, 4, 4, 8, 8,
+  8, 8, 4, 4,
+};
+
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(tonePin, channel);
+}
+
+void loop() {
+    for (int thisNote = 0; thisNote < 11; thisNote++) {
+      int noteDuration = 1000 / noteDurations[thisNote];
+      ledcWriteTone(channel, melody[thisNote]);
+      delay(noteDuration);
+      ledcWriteTone(channel, 0);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+    }
+}
